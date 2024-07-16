ab33dfd0a9dd1f5612b0310448ef12c8098d307c
chetanjadhav-dev <chetanjadhav.dev@gmail.com>
2024-07-16 11:39:43 +0530

    first commit

--- tables.py
+++ tables.py
@@ -5,9 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
+import csv
 
 website = "https://yokohama-atg.com/usa/tire-selector-yokohama-off-highway-tires/"
-path = "C:\\Users\\cheta\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"  # Change according to your pc's file path
+path = "C:\\Users\\cheta\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"  # Change according to your PC's file path
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 
@@ -70,12 +71,9 @@ try:
         for des in select_design:
             if des.text in values:
                 new_design.append(des)
-                print(des.text)
-
         return new_design
 
     designs = get_design(my_designs)
-    print(designs)
     for design in designs:
         print(f'For Design: {design.text}')
         design.click()
@@ -87,29 +85,61 @@ try:
         search_button.click()
         print('Search...')
 
-        # Try finding tables with both class names
+        # Wait for tables to become visible
         try:
-            find_tables = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, '//div[@class=" table-responsive"]'))
+            # Find the parent div element containing the tables you're interested in
+            data_in_mm = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@class="data_in_mm "]'))
             )
-            print('We found tables with class=" table-responsive"!')
-        except:
-            try:
-                find_tables = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, '//div[@class="table-responsive"]'))
-                )
-                print('We found tables with class="table-responsive"!')
-            except Exception as e:
-                print(f"An error occurred while fetching tables: {str(e)}")
-                continue
 
-        try:
-            tables = find_tables.find_elements(By.XPATH, '//table[@id="tblResultView"]')
-            print('we found',len(tables),'tables.')
+            # Find all tables within the parent div
+            tables_mm = data_in_mm.find_elements(By.XPATH, './/table[@id="tblResultView"]')
+
+            for i, table in enumerate(tables_mm):
+                # Extract header row
+                header_row = table.find_element(By.TAG_NAME, 'thead').find_elements(By.TAG_NAME, 'tr')
+                speeds = header_row[-1].find_elements(By.TAG_NAME, 'th')
+                print(f'Table {i+1}:\n\n', 'tr 1:\n\n',header_row[0].text, '\n\ntr 2:\n\n', header_row[1].text, '\n\ntr 3:\n\n', header_row[2].text, '\n\ntr 4:\n\n', [speed.text for speed in speeds])
+                print()
+                print('end...')
+                print()
+
+            # Print tables for demonstration
+            if len(tables_mm) > 1:
+                for i, table in enumerate(tables_mm):
+                    table_html = table.text
+                    print()
+                    print(f'Table {i+1} for "International Standard (mm - bar - kg - kmph)":\n\n', table_html)
+                    print()
+            else:
+                print('Tables not found!!!')
+
+            data_in_inch = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, '//div[@class="data_in_inch "]'))
+            )
+
+            # Find all table elements within the parent div
+            tables_inch = data_in_inch.find_elements(By.XPATH, './/table[@id="tblResultView"]')
+
+            # Execute JavaScript to change the display style to block (making it visible)
+            driver.execute_script("arguments[0].style.display = 'block';", data_in_inch)
+
+            # Print the HTML content of each table for demonstration
+            if len(tables_inch) > 1:
+                for i, table in enumerate(tables_inch):
+                    table_html = table.text
+                    print()
+                    print(f'Table {i+1} for "US Standard (inch - psi - lbs - mph)"):\n\n', table_html)
+                    print()
+            else:
+                print('tables not found!!!')
+
         except Exception as e:
-            print(f"An error occurred while processing tables: {str(e)}")
+            print(f"An error occurred while fetching tables 'tblResultView': {str(e)}")
+
+
 
 finally:
-    time.sleep(5)
+    time.sleep(10)
     driver.quit()
     print("All data has been collected and the browser is closed.")
